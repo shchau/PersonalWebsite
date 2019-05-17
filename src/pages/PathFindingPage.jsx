35e7622bcceae434da7898592593245b65a5099f
shchau <shchau1@ualberta.ca>
2019-05-16 20:19:48 -0600

    Line drawing animation prepared.

--- src/pages/PathFindingPage.jsx
+++ src/pages/PathFindingPage.jsx
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
+import SvgLines from 'react-mt-svg-lines';
 import Asteroid from '../components/Asteroid';
 
 const spawnLocations = [
-	['10%', '15%'], ['10%', '45%'], ['15%', '70%'],
-	['30%', '40%'], ['30%', '70%'], ['40%', '20%'],
-	['50%', '30%'], ['50%', '50%'], ['50%', '80%'],
-	['60%', '15%'], ['60%', '40%'], ['60%', '70%'],
-	['70%', '40%'], ['70%', '50%'], ['70%', '85%'],
-	['80%', '15%'], ['80%', '35%'], ['80%', '70%'] 
+	['10', '15'], ['10', '45'], ['15', '70'],
+	['30', '40'], ['30', '70'], ['40', '20'],
+	['50', '30'], ['50', '50'], ['50', '80'],
+	['60', '15'], ['60', '40'], ['60', '70'],
+	['70', '40'], ['70', '50'], ['70', '85'],
+	['80', '15'], ['80', '35'], ['80', '70'] 
 ];
 
 class PathFindingPage extends Component {	
@@ -17,6 +18,7 @@ class PathFindingPage extends Component {
 		this.state = {
 		};
 		this.generateAsteroids = this.generateAsteroids.bind(this);
+		this.drawPath = this.drawPath.bind(this);
 		this.beginPathFinder = this.beginPathFinder.bind(this);
 	}
 	
@@ -31,8 +33,10 @@ class PathFindingPage extends Component {
 		}
 		
 		return (selectedSpawns.map((index) => {
+			let leftPos = spawnLocations[index][0] + "%";
+			let topPos = spawnLocations[index][1] + "%";
 			return(
-				<span key={index} style={{position: 'absolute', left: spawnLocations[index][0], top: spawnLocations[index][1],}}>
+				<span key={index} style={{position: 'absolute', left: leftPos, top: topPos,}}>
 					<Asteroid id={index} beginPathFinder={this.beginPathFinder} />
 				</span>
 			
@@ -41,14 +45,39 @@ class PathFindingPage extends Component {
 	
 	}
 
+	drawPath(drawInstructions) {
+		return(
+			<SvgLines animate={4000} duration={6000}>
+				<svg viewBox="0 0 100 100">
+					<path d={drawInstructions} 
+					style={{stroke:"blue", strokeWidth: 0.2}}
+					fill="none"
+					/>
+				</svg>
+			</SvgLines>
+		);
+	}
+
+
 	beginPathFinder(index) {
+		// Begin path finding algorithm starting from spawnLocation at the selected index 
 		console.log(index);
+		
+		// Call drawPath given order returned by path finding algorithm 
+		//let drawInstructions = "M " + x1 + ", " + y1 + " L " + x2 + ", " + y2;
+		// REDUX time lol
+		let drawInstructions = "M 10, 15 L 30, 40";
+		return(this.drawPath(drawInstructions));
 	}
 
 	render() {
+	// Need to prevent call of generateAsteroids once path is made, OR
+	// don't allow user to select starting point for path finder.
 		return(	
 			<span>
-				{this.generateAsteroids()}
+				{this.generateAsteroids()}	
+				{this.beginPathFinder(0)} 
+				
 			</span>
 			)
     }
